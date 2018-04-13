f5930cb685bcffbb7ac73708ca86578c57e6faaa
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2018-04-13 20:34:39 +0200

    util/logger: add custom loggers

---
+++ util/logger.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"log"
+	"os"
+)
+
+var InfoLogger, WarningLogger, ErrorLogger *log.Logger
+
+func init() {
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
