69a8da15a7e1f3e8f541b75b14881dabfa283c70
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2018-04-14 10:42:34 +0200

    util/logger: change log format

--- util/logger.go
+++ util/logger.go
@@ -5,10 +5,14 @@ import (
 	"os"
 )
 
-var InfoLogger, WarningLogger, ErrorLogger *log.Logger
+var (
+	InfoLogger    *log.Logger
+	WarningLogger *log.Logger
+	ErrorLogger   *log.Logger
+)
 
 func init() {
-	InfoLogger = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(os.Stdout, "[WARNING] ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime)
+	WarningLogger = log.New(os.Stdout, "[WARNING] ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
 }
