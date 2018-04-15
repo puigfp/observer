99aee0d6fd29a8a8c4eae5aa1dd59729dc9eb81a
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2018-04-15 15:18:54 +0200

    fetch/type: add doc

--- fetch/type.go
+++ fetch/type.go
@@ -7,12 +7,12 @@ import (
 
 // metricPoint stores the information about how a request went
 type metricPoint struct {
-	timestamp    time.Time
-	website      string
-	responseTime int64
-	statusCode   int
-	status       string
-	success      bool
+	timestamp    time.Time //
+	website      string    // website.Name
+	responseTime int64     // response time, in nanoseconds (-1 if something went wrong before getting an HTTP response)
+	statusCode   int       // HTTP status code (-1 if something wrong happened before getting an HTTP response)
+	status       string    // either response.Status (ex: "200 OK", "404 Not Found") or a string describing what went wrong
+	success      bool      // true only when the response HTTP code is in [200, 400[
 }
 
 // metricsBuffer is a thread-safe metricPoint slice
