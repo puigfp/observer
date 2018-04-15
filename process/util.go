dd0f771c6edd519b896d62b1337e6927d3638a02
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2018-04-15 17:09:05 +0200

    process: add missing doc

--- process/util.go
+++ process/util.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// parseJSONNumber casts an interface{} wrapping a json.Number variable to int64
 func parseJSONNumber(number interface{}) (int64, bool) {
 	if number == nil {
 		return 0, true
@@ -32,6 +33,7 @@ func roundSub(t time.Time, d time.Duration) time.Time {
 	return res
 }
 
+// index finds the index of the first occurence of a string in a string slice
 func index(slice []string, str string) (int, bool) {
 	for i, elem := range slice {
 		if elem == str {
