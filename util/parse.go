697a938a0284cbe0c60cffceb5225d97d374c5b2
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2018-04-17 21:19:48 +0200

    util/parse: add func to parse JSON floats

--- util/parse.go
+++ util/parse.go
@@ -23,3 +23,22 @@ func ParseJSONNumber(number interface{}) (int64, bool) {
 
 	return numberInt, true
 }
+
+// ParseJSONFloat casts an interface{} wrapping a json.Number variable to int64
+func ParseJSONFloat(number interface{}) (float64, bool) {
+	if number == nil {
+		return 0, true
+	}
+
+	numberStr, ok := number.(json.Number)
+	if !ok {
+		return 0, false
+	}
+
+	numberFloat, err := strconv.ParseFloat(string(numberStr), 64)
+	if err != nil {
+		return 0, false
+	}
+
+	return numberFloat, true
+}
